6c8cc8b29c26b8d5dfa8b9ffb0bf2b28ae375e71
zhihua <2632335513@qq.com>
2021-02-24 07:12:46 -0800

    [Modified]commit dijkstra.c [Engineer]Wzh [Reviewer]Wzh

--- algorithm/dijkstra.c
+++ algorithm/dijkstra.c
@@ -1,66 +1,75 @@
 #include <stdio.h>
+#include <string.h>
 
-#define inf 100000
 #define V_SIZE 6
 
-void array_fill(int *array, int len, int val)
+int is_empty(int *array)
 {
-	for(int i = 0; i < len; i++)
+	for(int i = 0; i < V_SIZE; i++)
 	{
-		array[i] = val;
+		if(array[i] != 0)
+			return 0;
 	}
+
+	return 1;
 }
 
-void dijkstra(int graph[][V_SIZE], int n, int start, int dist[])
+void dijkstra(int graph[][V_SIZE], int start)
 {
-	int *shortest_path = (int *)malloc(sizeof(int) * n);
-	int *mark=(int *)malloc(sizeof(int) * n);
-	int *path = (int *)malloc(sizeof(int) * n);
+	int visit[V_SIZE] = {0};
+	int parent[V_SIZE] = {0};
+	int frontier[V_SIZE] = {0};
+	int next[V_SIZE] = {0};
+	int dist[V_SIZE] = {0};
 
-	array_fill(mark, n, 0);
-	array_fill(dist, n, inf);
-
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < V_SIZE; i++)
 	{
-		dist[i] = graph[start][i];
-		if(i != start && dist[i] < inf) 
-			path[i] = start;
-		else 
-			path[i] = -1;
+		dist[i] = 1000;
 	}
 
-	mark[start] = 1;
-	
-	while(1)
+	frontier[start] = 1;
+	parent[start] = start;
+	dist[start] = 0;
+	while(!is_empty(frontier))
 	{
-		min = inf;
-		v = -1;
-		for(int i = 0; i < n; i++)
+		for(int i = 0; i < V_SIZE; i++)
 		{
-			if(!mark[i])
+			if(frontier[i] == 0)
+				continue;
+
+			memset(next, 0, sizeof(next));
+			for(int j = 0;  j < V_SIZE; j++)
 			{
-				if(dist[i] < min)
+				if(graph[i][j] == -1 && graph[i][j] != 0)
+					continue;
+
+				if(visit[j] != 1)
+					next[j] = 1;
+
+				if(dist[j] > graph[i][j] + dist[i])
 				{
-					min = dist[i];
-					v = i;
+					dist[j] =  graph[i][j] + dist[i];
+					parent[j] = i;
 				}
 			}
+			visit[i] = 1;
 		}
-
-		if(v == -1)
-		greak;:
+		memcpy(frontier, next, sizeof(frontier));
 	}
+
+	for(int i = 0; i < V_SIZE; i++)
+		printf("%d %d %d\n", i, dist[i], parent[i]);
 }
 
 int main()
 {
-	int dist[V_SIZE];
 	int W[V_SIZE][V_SIZE] = {
-		{ 0, 1, 4, inf, inf ,inf },
-		{ 1, 0, 2, 7, 5, inf },
-		{ 4, 2, 0, inf, 1, inf },
-		{ inf, 7, inf, 0, 3, 2 },
-		{ inf, 5, 1, 3, 0, 6 },
-		{ inf, inf, inf, 2, 6, 0 }
+		{ 0, 1, 4, -1, -1 ,-1 },
+		{ 1, 0, 2, 7, 5, -1 },
+		{ 4, 2, 0, -1, 1, -1 },
+		{ -1, 7, -1, 0, 3, 2 },
+		{ -1, 5, 1, 3, 0, 6 },
+		{ -1, -1, -1, 2, 6, 0 }
 	};
+	dijkstra(W, 0);
 }
